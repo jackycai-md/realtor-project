e632572b43fd53b97da05fcf468883907572213c
Jacky Cai <mdjacky.cai@gmail.com>
2021-09-14 20:36:35 -0400

    format time in feature page

--- src/app/FeaturePage.jsx
+++ src/app/FeaturePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fetchEarthQuakeDataById } from './services';
 import InfoRow from './InfoRow';
+import { formatTime } from './utils';
 
 const INFORMATIONS = [
   { key: 'title', label: 'title' },
@@ -22,12 +23,22 @@ export default function FeaturePage({ featureId }) {
 
   const properties = feature.properties || {};
 
+  const getFormattedValue = (key) => {
+    const value = properties[key];
+
+    if (key === 'time') {
+      return formatTime(value);
+    }
+
+    return value;
+  };
+
   return (
     <div>
       <h2 className="page-title">{properties.title}</h2>
       <div className="feature-details">
         {INFORMATIONS.map((info) => (
-          <InfoRow label={info.label} key={info.key} value={properties[info.key]} />
+          <InfoRow label={info.label} key={info.key} value={getFormattedValue(info.key)} />
         ))}
       </div>
     </div>
